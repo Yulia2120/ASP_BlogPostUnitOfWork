e0821b4593e9de083c0a2b51b9052cb0bf9d8049
Yulia2120 <ket-dz@rambler.ru>
2022-10-25 13:49:16 +0300

    connectionstring

--- ASP_BlogPostUnitOfWork/Program.cs
+++ ASP_BlogPostUnitOfWork/Program.cs
@@ -1,7 +1,11 @@
+using DAL;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+var connectionString = builder.Configuration.GetConnectionString("DbConnection");
+builder.Services.AddDbContext<BlogContext>(options => options.UseSqlServer(connectionString));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
